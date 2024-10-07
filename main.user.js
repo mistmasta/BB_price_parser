c41c2ca34708d8e24a0844a7456eaa3d718c6f94
mistmasta <b-unin@yandex.com>
2024-10-07 20:28:23 +0300

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Данные о товарах Dota2
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      1.0
 // @description  Получение данных о товарах с карточек
 // @author       mistmasta
 // @match        https://betboompass.com/shop?order=DESC&is_meta=false&type_game=dota2
@@ -12,7 +12,7 @@
 // @connect      steamcommunity.com
 // @run-at       document-idle
 // ==/UserScript==
-
+//test
 (function() {
     'use strict';
 
